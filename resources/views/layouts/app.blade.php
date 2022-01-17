76297873b8e421c3ef2c77edfbe1cdb265db7ddd
Stuart Corns <stuart.corns@tandonta.com>
2022-01-17 21:18:15 +0000

    Users

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
     @yield('css')
 </head>
-<body class="dark-theme">
+<body class="">
     <x-layouts.sidebar />
     <x-layouts.topbar />
     @yield('content')
