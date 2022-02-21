696b5f3819d4b038064c06f7ffba586ddc2bd218
Stuart Corns <stuart.corns@tandonta.com>
2022-02-21 13:23:47 +0000

    COnfig

--- app/Http/Controllers/TimesheetController.php
+++ app/Http/Controllers/TimesheetController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Timesheet;
 use App\Models\Client;
 use Illuminate\Http\Request;
+use App\Jobs\SubmitTimesheet;
 
 class TimesheetController extends Controller
 {
