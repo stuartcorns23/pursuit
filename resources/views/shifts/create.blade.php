fb90b9d2d3557f171ff305e6133449a8e57ab84a
Stuart Corns <stuart.corns@tandonta.com>
2022-02-24 11:34:57 +0000

    Availability Alerts

--- resources/views/shifts/create.blade.php
+++ resources/views/shifts/create.blade.php
@@ -231,10 +231,11 @@
     }
 
     function checkAvailability(){
+        /* alert('changed');
         let date = shiftDate.value;
         console.log(date);
         let ops = document.querySelectorAll('[name="user_id[]"]');
-        console.log(ops);
+        console.log(ops); */
 
 
         /* let ops = [];
