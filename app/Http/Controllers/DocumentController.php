8d262f9c071b5418785c946f53788061b6bddbe3
Stuart Corns <stuart.corns@tandonta.com>
2022-02-28 11:39:14 +0000

    Document Upload

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -14,7 +14,7 @@ class DocumentController extends Controller
      */
     public function index()
     {
-        //
+        return view('documents.create');
     }
 
     /**
@@ -24,7 +24,7 @@ class DocumentController extends Controller
      */
     public function create()
     {
-        //
+        return view('documents.create');
     }
 
     /**
