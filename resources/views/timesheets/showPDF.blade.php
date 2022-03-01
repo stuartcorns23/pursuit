be673d289a9b58040b01b6c8b0aaa825cef1356a
Stuart Corns <stuart.corns@tandonta.com>
2022-03-01 10:29:26 +0000

    Timesheet PDF

--- resources/views/timesheets/showPDF.blade.php
+++ resources/views/timesheets/showPDF.blade.php
@@ -14,7 +14,7 @@
         }
 
         #header{
-            background-color: #454777;
+            background-color: #1d6fb8;
             width: 100%;
             margin-bottom: 30px;
             color: #fff;
@@ -34,7 +34,7 @@
 
         .table th{
             padding: 2px;
-            background-color: #454777;
+            background-color: #1d6fb8;
             color: #FFF;
             border: solid 1px #999;
             text-align: center;
@@ -67,8 +67,8 @@
     <header id="header">
         <table width="100%"></i>
             <tr>
-                <td align="left" style="padding-left:10px;" width="20%"><img id="logo" src="{{ asset('images/apollo-logo.jpg') }}" alt="Apollo Assets Manager"></td>
-                <td align="left">Apollo Asset Management<br><span class="small">A Central Learning Partnership Trust (CLPT) System &copy; 2021</span>
+                <td align="left" style="padding-left:10px;" width="20%"><img id="logo" src="{{ asset('images/pursuit-tmr.jpg') }}" alt="Pursuit TMR"></td>
+                <td align="left">Apollo Asset Management<br><span class="small">Pursuit Traffic Management Recruitment Ltd &copy; {{\Carbon\Carbon::now()->format('Y')}}</span>
                     <br><strong>@yield('page')</strong>
                 </td>
                 <td align="right" style="padding-right: 10px;">
