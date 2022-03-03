26868ea57e4fbd20e49cc3e476ef9775ecdeca99
Stuart Corns <stuart.corns@tandonta.com>
2022-03-03 21:19:47 +0000

    Document Upload

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -45,7 +45,8 @@ class DocumentController extends Controller
 
         $fileName = $request->file->getClientOriginalName();
 
-        $document = Document::create(['name' => $request->name, 'type_id' => $request->type_id, 'expiry' => $request->expiry, 'path' => 'path', 'user_id' => auth()->user()->id])->save();
+        $document =  new Document;
+        $document->fill(['name' => $request->name, 'type_id' => $request->type_id, 'expiry' => $request->expiry, 'path' => 'path', 'user_id' => auth()->user()->id])->save();
         Storage::disk('public')->putFile('documents/new-doc.pdf', $request->file);
         $document->addMediaFromUrl(Storage::disk('public')->url('documents/new-doc.pdf'))->toMediaCollection();
         return redirect('documents.index');
