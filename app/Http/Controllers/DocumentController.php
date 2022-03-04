7671be00b208c060794532387342d3d169f2843f
Stuart Corns <stuart.corns@tandonta.com>
2022-03-04 12:50:31 +0000

    Approved Documents

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -9,34 +9,19 @@ use Illuminate\Support\Facades\Storage;
 
 class DocumentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         $documents = Document::all();
         return view('documents.view', compact('documents'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         $types = Type::all();
         return view('documents.create', compact('types'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $request->validate([
@@ -55,48 +40,38 @@ class DocumentController extends Controller
         return redirect('documents.index');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Document  $document
-     * @return \Illuminate\Http\Response
-     */
+
     public function show(Document $document)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Document  $document
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Document $document)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Document  $document
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Document $document)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Document  $document
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Document $document)
     {
         //
+    }
+
+    public function approve(Document $document){
+        $document->checked = 1;
+        $document->checked_by = auth()->user()->id;
+        $document->checked_date = \Carbon\Carbon::now();
+        $document->save();
+
+        session()->flash('success_message', 'You have approved the document!');
+        return redirect(route('documents.index'));
+    }
+
+    public function reject(Document $document){
+        
     }
 }
--- app/Models/Document.php
+++ app/Models/Document.php
@@ -12,7 +12,7 @@ class Document extends Model implements HasMedia
     use HasFactory;
     use InteractsWithMedia;
 
-    protected $fillable = ['type_id', 'user_id', 'expiry', 'checked', 'checked_by', 'path'];
+    protected $fillable = ['type_id', 'user_id', 'expiry', 'checked', 'checked_date', 'checked_by', 'path'];
 
     public function user(){
         return $this->belongsTo(User::class);
