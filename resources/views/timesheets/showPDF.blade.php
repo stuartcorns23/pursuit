adeab1c3aa268e3d51e272ff8a1eda05eee7b868
Stuart Corns <stuart.corns@tandonta.com>
2022-03-21 20:17:15 +0000

    Timesheet Update

--- resources/views/timesheets/showPDF.blade.php
+++ resources/views/timesheets/showPDF.blade.php
@@ -125,8 +125,8 @@
                 <td>{{$shift->start}}</td>
                 <td>{{$shift->end}}</td>
                 @php
-                    $start_time = $day->format('d-m-Y')." ".$shift->$start;
-                    $end_time = $day->format('d-m-Y')." ".$request->$end;
+                    $start_time = $shift->date." ".$shift->$start;
+                    $end_time = $shift->date." ".$request->$end;
                     //get the rate variable
                     if($shift->pay_ype == 'per-hour'){
                         $diffInHours = \Carbon\Carbon::parse($start_time)->diffInHours(\Carbon\Carbon::parse($end_time));
