dfab3a8ffaf36e703f14cdea094f7b5d3ac2e3fa
Stuart Corns <stuart.corns@tandonta.com>
2022-03-23 23:24:46 +0000

    TimesheetsExpenses

--- resources/views/timesheets/view.blade.php
+++ resources/views/timesheets/view.blade.php
@@ -59,6 +59,9 @@
                                     @if(!empty($timesheet->getFirstMedia('timesheets')))
                                     <a class="dropdown-item" href="{{$timesheet->getFirstMedia('timesheets')->getFullUrl()}}">Download Timesheet</a>
                                     @endif
+                                    @if(!empty($timesheet->getFirstMedia('expenses')))
+                                    <a class="dropdown-item" href="{{$timesheet->getFirstMedia('expenses')->getFullUrl()}}">Download Expenses</a>
+                                    @endif
                                     <a class="dropdown-item" href="{{route('timesheets.edit', $timesheet->id)}}">Edit</a>
                                     <form id="form{{$timesheet->id}}" action="{{ route('users.destroy', $timesheet->id) }}" method="POST">
                                         @csrf
