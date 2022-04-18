2ee066a0f8ae367ca81d5dfb448b489edc5ca9b6
Stuart Corns <stuart.corns@tandonta.com>
2022-04-18 12:13:20 +0100

    Scheduled Jobs - Alert Operatives Available

--- app/Jobs/NotifyAvailableOperatives.php
+++ app/Jobs/NotifyAvailableOperatives.php
@@ -8,8 +8,8 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use App\Notifications\SendAvailableUsers;
-use App\Models\User;
+use \App\Notifications\SendAvailableUsers;
+use \App\Models\User;
 
 class NotifyAvailableOperatives implements ShouldQueue
 {
