2c4875136d355bc04f50b26bd3ff29adae262986
Stuart Corns <stuart.corns@tandonta.com>
2022-04-19 10:12:57 +0100

    Operatives Email

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Jobs\NotifyAvailableOperatives;
+use App\Jobs\AlertOperativesAvailability;
 use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
@@ -22,6 +23,7 @@ class Kernel extends ConsoleKernel
         $schedule->job(new AlertOperativesTimesheet)->weekly()->mondays()->at('11:00');
         $schedule->job(new NotifyDailyShifts)->daily()->at('11:00'); */
         $schedule->job(new NotifyAvailableOperatives)->everyFiveMinutes();
+        $schedule->job(new AlertOperativesAvailability)->everyFiveMinutes();
     }
 
     /**
--- app/Notifications/AlertOperativesAvailability.php
+++ app/Notifications/AlertOperativesAvailability.php
@@ -21,16 +21,11 @@ class AlertOperativesAvailability extends Notification
     {        
         // OR return a SnsMessage passing the arguments via `create()` or `__construct()`:
         return SnsMessage::create([
-            'body' => "Your Account has been approved! If you have verified your email you can now access your account at https://www.pursuit-tmr.co.uk/schedule/".Carbon::now()->format('m\/Y'),
+            'body' => "You haven't set any availabilty for next week. If you are available please let us know at - https://www.pursuit-tmr.co.uk/schedule/".Carbon::now()->format('m\/Y')." as soon as possible to
+                prevent any delays. Thank you ",
             'transactional' => true,
             'sender' => 'Pursuit-TMR',
         ]); 
-
-        // OR create the object with or without arguments and then use the fluent API:
-        return SnsMessage::create()
-            ->body("Your Account has been approved! If you have verified your email you can now access your account at https://www.pursuit-tmr.co.uk/schedule/".Carbon::now()->format('m\/Y'))
-            ->promotional()
-            ->sender('Pursuit TMR');
     }
 }
 
