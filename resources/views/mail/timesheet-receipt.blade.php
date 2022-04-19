e8f53d6e38fdd836f29854316684c946d44de64c
Stuart Corns <stuart.corns@tandonta.com>
2022-04-19 11:03:35 +0100

    Operatives Email

--- resources/views/mail/timesheet-receipt.blade.php
+++ resources/views/mail/timesheet-receipt.blade.php
@@ -4,7 +4,7 @@
 @section('titlePT2', 'Welcome Pursuit Traffic Management')
 
 @section('content')
-    <p>Recipt</p>
+    <p>Thank you for submitting you timesheet, it has been received and will be processed soon. A copy of you timesheet and expenses (If submitted have bee attached to this email). Any problems please let us know</p>
 
     <p>Kind Regards</p>
     <p>The Pursuit TMR Team</p>
